738f3b5e080b16bc434a2143e455254bafddff19
Akshay Sawant <akshay@cloudwalker.tv>
2021-09-08 22:17:35 +0530

    Find Screen: - created the ui and its required fields into the view model class file - configure the view model class file into both ui and logic file - replaced with on view created function in the FindFragment.kt file

--- app/src/main/java/com/devdays/bloodcare/ui/find/FindFragment.kt
+++ app/src/main/java/com/devdays/bloodcare/ui/find/FindFragment.kt
@@ -7,13 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.devdays.bloodcare.databinding.FindFragmentBinding
+import com.devdays.bloodcare.util.EventObserver
 import com.devdays.bloodcare.util.getViewModelFactory
+import com.devdays.bloodcare.util.setUpSnackbar
+import com.google.android.material.snackbar.Snackbar
 
 class FindFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = FindFragment()
-    }
 
     private lateinit var mFindFragmentBinding: FindFragmentBinding
     private val mFindViewModel by viewModels<FindViewModel> { getViewModelFactory() }
@@ -27,8 +26,23 @@ class FindFragment : Fragment() {
         return mFindFragmentBinding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         mFindFragmentBinding.lifecycleOwner = viewLifecycleOwner
+
+        setUpFindSnackbar()
+        setUpFindNavigation()
+    }
+
+    private fun setUpFindSnackbar() {
+        view?.setUpSnackbar(
+            this@FindFragment.viewLifecycleOwner,
+            mFindViewModel.mFindSnackbarMessage,
+            Snackbar.LENGTH_SHORT
+        )
+    }
+
+    private fun setUpFindNavigation() {
+        mFindViewModel.mFindSubmitRequestEvent.observe(viewLifecycleOwner, EventObserver {})
     }
 }
--- app/src/main/java/com/devdays/bloodcare/ui/find/FindViewModel.kt
+++ app/src/main/java/com/devdays/bloodcare/ui/find/FindViewModel.kt
@@ -1,7 +1,52 @@
 package com.devdays.bloodcare.ui.find
 
+import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.devdays.bloodcare.R
+import com.devdays.bloodcare.util.Event
 
 class FindViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val mFindFullName: ObservableField<String> = ObservableField("")
+    val mFindMobileNumber: ObservableField<String> = ObservableField("")
+    val mFindRelation: ObservableField<String> = ObservableField("")
+    val mFindBloodGroup: ObservableField<String> = ObservableField("")
+    val mFindLocation: ObservableField<String> = ObservableField("")
+
+    private val _findSnackbarMessage = MutableLiveData<Event<Int>>()
+    val mFindSnackbarMessage: LiveData<Event<Int>> = _findSnackbarMessage
+
+    private val _findSubmitRequestEvent = MutableLiveData<Event<Unit>>()
+    val mFindSubmitRequestEvent: LiveData<Event<Unit>> = _findSubmitRequestEvent
+
+    fun showFindSnackbar(mMessage: Int) {
+        _findSnackbarMessage.value = Event(mMessage)
+    }
+
+    fun onClickFindSubmitRequest() {
+        when {
+            mFindFullName.get()?.isEmpty() == true -> {
+                showFindSnackbar(R.string.text_error_full_name)
+            }
+            mFindMobileNumber.get()?.isEmpty() == true -> {
+                showFindSnackbar(R.string.text_error_field_empty)
+            }
+            mFindMobileNumber.get()?.length!! < 10 -> {
+                showFindSnackbar(R.string.text_label_invalid_mobile_number)
+            }
+            mFindRelation.get()?.isEmpty() == true -> {
+                showFindSnackbar(R.string.text_error_relation)
+            }
+            mFindBloodGroup.get()?.isEmpty() == true -> {
+                showFindSnackbar(R.string.text_error_blood_group)
+            }
+            mFindLocation.get()?.isEmpty() == true -> {
+                showFindSnackbar(R.string.text_error_location)
+            }
+            else -> {
+                _findSubmitRequestEvent.value = Event(Unit)
+            }
+        }
+    }
 }
