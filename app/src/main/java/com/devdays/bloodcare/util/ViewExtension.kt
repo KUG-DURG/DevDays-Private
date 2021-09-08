11885e2e299051914dc5a6c4026952ed9bf15fd0
Akshay Sawant <akshay@cloudwalker.tv>
2021-09-08 19:11:11 +0530

    removed unwanted glide and spanning text code

--- app/src/main/java/com/devdays/bloodcare/util/ViewExtension.kt
+++ app/src/main/java/com/devdays/bloodcare/util/ViewExtension.kt
@@ -1,24 +1,13 @@
 package com.devdays.bloodcare.util
 
 import android.content.Context
-import android.graphics.Typeface
-import android.os.Build
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
-import android.text.style.StyleSpan
 import android.view.Gravity
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.widget.AppCompatImageView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-import com.bumptech.glide.Glide
-import com.devdays.bloodcare.R
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
@@ -57,40 +46,4 @@ fun View.setUpSnackbar(
             showSnackbar(context.getString(it), mTimeLength)
         }
     })
-}
-
-@BindingAdapter("spannableString")
-fun onSpanString(mTextView: TextView, mData: String) {
-    val mWordToSpan = SpannableString(mData)
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        mWordToSpan.setSpan(
-            ForegroundColorSpan(mTextView.context.resources.getColor(R.color.redDark, null)),
-            23,
-            30,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        mWordToSpan.setSpan(
-            StyleSpan(Typeface.BOLD),
-            23,
-            30,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-    }
-
-    mTextView.text = mWordToSpan
-}
-
-@BindingAdapter("loadImageById")
-fun onLoadImage(mAppCompatImageView: AppCompatImageView, mImageId: Int) {
-    Glide.with(mAppCompatImageView.context)
-        .load(mImageId)
-        .into(mAppCompatImageView)
-}
-
-@BindingAdapter("loadImageByUrl")
-fun onLoadImageByUrl(mAppCompatImageView: AppCompatImageView, mImageUrl: String) {
-    Glide.with(mAppCompatImageView.context)
-        .load(mImageUrl)
-        .into(mAppCompatImageView)
 }
