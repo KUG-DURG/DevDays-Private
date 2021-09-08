606e2c9da012ae315fd3bd891758dc89c56cc261
Akshay Sawant <akshay@cloudwalker.tv>
2021-09-08 18:27:50 +0530

    removed "first time launch" code from the SharedPreferenceUtils.kt file

--- app/src/main/java/com/devdays/bloodcare/util/SharedPreferenceUtils.kt
+++ app/src/main/java/com/devdays/bloodcare/util/SharedPreferenceUtils.kt
@@ -5,17 +5,12 @@ import android.content.Context
 class SharedPreferenceUtils(private var mContext: Context) {
 
     private val LOGIN_PREFERENCE_FILE = "login_preference_file"
-    private val FIRST_TIME_LAUNCH = "first_time_launch"
     private val LOGIN_TOKEN = "login_token"
 
     private var mPreferenceEditor =
         mContext.getSharedPreferences(LOGIN_PREFERENCE_FILE, Context.MODE_PRIVATE)
 
-    var mIsAppLaunchedForFirstTime: Boolean
-        get() = mPreferenceEditor.getBoolean(FIRST_TIME_LAUNCH, true)
-        set(value) = mPreferenceEditor.edit().putBoolean(FIRST_TIME_LAUNCH, value).apply()
-
-    var mLoginToken: String?
-        get() = mPreferenceEditor.getString(LOGIN_TOKEN, "")
-        set(value) = mPreferenceEditor.edit().putString(LOGIN_TOKEN, value).apply()
+    var mLoginToken: Boolean
+        get() = mPreferenceEditor.getBoolean(LOGIN_TOKEN, false)
+        set(value) = mPreferenceEditor.edit().putBoolean(LOGIN_TOKEN, value).apply()
 }
